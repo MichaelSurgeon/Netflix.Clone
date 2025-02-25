aa0797749aee6bee94828bd22b4802b2038d2492
Michael Surgeon <m.surgeon@craneware.com>
2025-02-25 23:24:06 +0000

    starting the netflix home page

--- netflix/src/App/App.js
+++ netflix/src/App/App.js
@@ -1,16 +1,28 @@
-import logo from '../Assets/logo.svg';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
+import Home from '../Pages/Home/Home';
+import Logo from '../Assets/logo.svg';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Hello World!
-        </p>
-      </header>
-    </div>
-  );
+      <Router>
+        <div className="App">
+          <div className="App-Navigation">
+            <nav>
+              <img className="App-Logo" src={Logo} alt=""/>
+              <ul className='App-Navigation-List'>
+                <li>Home</li>
+              </ul>
+            </nav>
+          </div>
+          <div className="Content">
+            <Routes>
+              <Route path="/" element={<Home />} />
+            </Routes>
+          </div>
+        </div>
+      </Router>  
+    );
 }
 
 export default App;
---
+++ netflix/src/Pages/Home/Home.js
@@ -0,0 +1,15 @@
+import '../../App/App.css';
+
+function Home() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          How is your day
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default Home;
